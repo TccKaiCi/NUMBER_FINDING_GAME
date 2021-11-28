46200123686097e54dd67555f47729f9231d3a2a
trong7a2002 <tienem123.com@gmail.com>
2021-11-28 16:18:00 +0700

    -:) chu he ca ngay
    - update server multithread
    - Để yên client vs server cũ cho t

---
+++ NUMBER_FINDING_GAME/src/main/java/com/server/number_finding_game/ChatServerThread.java
@@ -0,0 +1,69 @@
+package com.server.number_finding_game;
+
+import com.server.number_finding_game.Memory;
+import com.server.number_finding_game.NewServer;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class ChatServerThread extends Thread {
+
+    private NewServer newServer = null;
+    private Socket socket = null;
+    private SocketAddress ID = null;
+    private BufferedInputStream bis = null;
+    private DataInputStream dis = null;
+    private BufferedOutputStream bos = null;
+    private DataOutputStream dos = null;
+
+    public ChatServerThread(NewServer _New_server, Socket _socket) {
+        super();
+        newServer = _New_server;
+        socket = _socket;
+        ID = socket.getRemoteSocketAddress();
+    }
+
+    public SocketAddress getID() {
+        return ID;
+    }
+
+    public void send(String message) {
+        try {
+            dos.writeUTF(message);
+            dos.flush();
+        } catch (IOException e) {
+            System.out.println("Client " + socket.getRemoteSocketAddress() + " error sending : " + e.getMessage());
+            newServer.remove(ID);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Client " + socket.getRemoteSocketAddress() + " connected to server...");
+
+            bis = new BufferedInputStream(socket.getInputStream());
+            dis = new DataInputStream(bis);
+            bos = new BufferedOutputStream(socket.getOutputStream());
+            dos = new DataOutputStream(bos);
+            
+//            CHANGE ATTIBUUE 
+            Memory.playerConnect = true;
+            Memory.playerConnectName = socket.getRemoteSocketAddress().toString();
+            
+            while (true) {
+                newServer.handle(ID, dis.readUTF());
+            }
+        } catch (IOException e) {
+            newServer.remove(ID);
+        }
+    }
+
+    public void close() throws IOException {
+        System.out.println("Client " + socket.getRemoteSocketAddress() + " disconnect from server...");
+        socket.close();
+        dis.close();
+        dos.close();
+    }
+}
