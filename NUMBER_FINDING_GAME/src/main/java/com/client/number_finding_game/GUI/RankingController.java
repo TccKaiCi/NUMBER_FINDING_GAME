372b424772056ccc5368cbbea17c2f7b295e4885
Just another Dev <60642132+prohzgod@users.noreply.github.com>
2021-12-13 10:30:46 +0700

    Add ranking stage

--- NUMBER_FINDING_GAME/src/main/java/com/client/number_finding_game/GUI/RankingController.java
+++ NUMBER_FINDING_GAME/src/main/java/com/client/number_finding_game/GUI/RankingController.java
@@ -1,19 +1,98 @@
 package com.client.number_finding_game.GUI;
 
+import com.DTO.Ranking;
+import com.server.number_finding_game.Memory;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.math.BigDecimal;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import com.client.number_finding_game.GUI.WaitingRoomController;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 public class RankingController implements Initializable {
     @FXML
-    Label lbl_uid, lbl_name, lbl_point, lbl_winRate;
+    Button btn_back;
+    @FXML
+    VBox vbox;
+    @FXML
+    ScrollPane scroll;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        btn_back.setOnAction(this::setBtn_back);
+        String[] arr = Memory.messenger.split(";");
+        if (arr[0].contains("RankingData")) {
+            Ranking listRank = new Ranking();
+            listRank.getJsonRankTable(arr[1]);
+            listRank = listRank.handleRank();
+            listRank.sortPoint();
 
+            for (Ranking dto : listRank.getList()) {
+                HBox playerPane = new HBox();
+                Label uid = new Label(dto.getUID());
+                Label name = new Label(dto.getName());
+                Label point = new Label(String.valueOf(dto.getPoint()));
+                Label winRate = new Label(dto.getWinRate());
+                System.out.println(uid.getText() + name.getText() + point.getText() + winRate.getText());
+                Insets insets = new Insets(11, 0, 0, 5);
+                uid.setPadding(insets);
+                name.setPadding(insets);
+                point.setPadding(insets);
+                winRate.setPadding(insets);
+                uid.setPrefWidth(130);
+                uid.setPrefHeight(28);
+                name.setPrefWidth(155);
+                name.setPrefHeight(28);
+                point.setPrefWidth(70);
+                point.setPrefHeight(28);
+                winRate.setPrefWidth(81);
+                winRate.setPrefHeight(28);
+                uid.setFont(new Font(19));
+                name.setFont(new Font(19));
+                point.setFont(new Font(19));
+                winRate.setFont(new Font(19));
+                uid.setTextFill(Color.GOLD);
+                name.setTextFill(Color.GOLD);
+                point.setTextFill(Color.GOLD);
+                winRate.setTextFill(Color.GOLD);
+
+                playerPane.getChildren().addAll(uid, name, point, winRate);
+                playerPane.setPrefHeight(50);
+                playerPane.setPrefWidth(300);
+                playerPane.setStyle("-fx-background-color: red; -fx-background-radius: 2;");
+                HBox.setMargin(playerPane, new Insets(30, 30, 0, 30));
+                vbox.getChildren().add(playerPane);
+                vbox.setSpacing(10);
+                vbox.setPadding(new Insets(10));
+
+            }
+            scroll.setContent(vbox);
+
+
+        }
     }
+
+    public void setBtn_back(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
 
 }
